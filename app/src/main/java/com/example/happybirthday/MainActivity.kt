658e0fbd735a0849b32ca6b6536fec08afd77b73
Helen Evans <helen.evans@and.digital>
2022-01-28 11:50:18 +0000

    switch between different backgrounds

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -16,13 +16,23 @@ class MainActivity : AppCompatActivity() {
         // in a variable
         val button = findViewById<Button>(R.id.button)
         val backgroundImage = findViewById<ImageView>(R.id.imageView)
+        var switch: Int = 1
 
         // operations to be performed
         // when user tap on the button
         button?.setOnClickListener()
         {
-            // toggling visibility of background image
-            backgroundImage.isVisible = !backgroundImage.isVisible
+            switch = (switch + 1)%4;
+            //can also use to set variable
+            val drawableResource = when (switch) {
+                1 -> R.drawable.androidparty1
+                2 -> R.drawable.androidparty2
+                3 -> R.drawable.androidparty3
+                0 -> R.drawable.androidparty4
+                else -> R.drawable.androidparty1
+            }
+
+            backgroundImage.setImageResource(drawableResource);
         }
 
     }
